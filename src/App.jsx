f34c3367b26f0eb7d16533b79b8ac35a51883ee9
Charles Bryant <cwbryant25@yahoo.com>
2024-02-13 14:04:22 -0800

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,39 @@
 import { useState } from "react";
 import { puppyList } from "./data.js";
 import "./App.css";
+import "./index.css";
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
-
-  console.log("puppyList: ", puppyList);
-
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  // console.log("puppyList: ", puppyList);
+  console.log(featuredPup);
   return (
     <>
       <div>
         {puppies.map((puppy) => {
-          return <p>{puppy.name}</p>;
+          return (
+            <p
+              onClick={() => {
+                console.log("puppy id: ", puppy.id);
+                setFeatPupId(puppy.id);
+              }}
+              key={puppy.id}
+            >
+              {puppy.name}
+            </p>
+          );
         })}
+        {featPupId && (
+          <div>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
       </div>
     </>
   );
